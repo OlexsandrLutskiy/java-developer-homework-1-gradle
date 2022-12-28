710d4b542478a3da110471d33aba84c32fa70e36
OlexsandrLutskiy <alexsandr.ivfr@gmail.com>
2022-12-28 21:14:51 +0200

    Add user print in gson

---
+++ app/src/main/java/javadeveloper/User.java
@@ -0,0 +1,11 @@
+package javadeveloper;
+
+public class User {
+    private String name;
+    private String lastname;
+
+    public User(String name, String lastname) {
+        this.name = name;
+        this.lastname = lastname;
+    }
+}
